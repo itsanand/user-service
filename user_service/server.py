c5f72de58e8ac2bbf975d1cfb8876bd14ba67190
itsanand <cu.18bcs2174@gmail.com>
2023-10-14 21:00:54 +0530

    feat(user-service): initiated user service server

---
+++ user_service/server.py
@@ -0,0 +1,15 @@
+"""Handles User App Server"""
+from starlette.applications import Starlette
+from starlette.routing import Route
+from user_service.endpoints.user_endpoint import UserEndpoint
+
+user_endpoint: UserEndpoint = UserEndpoint()
+
+routes: list[Route] = [
+    Route("/user", user_endpoint.create_user, methods=["POST"]),
+    Route("/user/{id}", user_endpoint.update_user, methods=["PATCH"]),
+    Route("/user/{id}", user_endpoint.delete_user, methods=["DELETE"]),
+    Route("/user/{id}", user_endpoint.fetch_user, methods=["GET"]),
+]
+
+app: Starlette = Starlette(routes=routes)
