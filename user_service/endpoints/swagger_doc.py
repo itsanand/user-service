d5c0f37d8e150fa821acb96345621c60dfd1e915
itsanand <cu.18bcs2174@gmail.com>
2023-10-20 21:41:42 +0530

    fix(swagger): fixed swagger doc file path

--- user_service/endpoints/swagger_doc.py
+++ user_service/endpoints/swagger_doc.py
@@ -9,7 +9,7 @@ class SwaggerDoc:
     async def swagger_ui(self, _: Request) -> HTMLResponse:
         """Handles html snippet for swagger doc"""
 
-        with open(r"/user_service/static/swagger_ui.html", encoding="utf-8") as file:
+        with open("./user_service/static/swagger_ui.html", encoding="utf-8") as file:
             html_code: str = file.read()
         return HTMLResponse(html_code)
 
@@ -17,5 +17,5 @@ class SwaggerDoc:
         """Handles yaml code for swagger doc"""
 
         return FileResponse(
-            path=r"/user_service/static/swagger.yaml", filename="openapi.yaml"
+            path="./user_service/static/swagger.yaml", filename="openapi.yaml"
         )
